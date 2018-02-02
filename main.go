1269a3c61f277171172986f3d64542fd9e8c4741
Thomas Barton <thomasryanbarton@gmail.com>
2018-02-02 15:14:33 -0800

    Extract file garbage collector package

--- main.go
+++ main.go
@@ -16,7 +16,6 @@ import (
 	"github.com/image-server/image-server/logger/statsd"
 	"github.com/image-server/image-server/paths"
 	"github.com/image-server/image-server/uploader"
-	"path/filepath"
 )
 
 // configT collects all the global state of the logging setup.
@@ -204,44 +203,6 @@ func initializeUploader(sc *core.ServerConfiguration) {
 		log.Println("EXITING: Unable to initialize uploader: ", err)
 		os.Exit(2)
 	}
-}
-
-func initializeFileCleaner(sc *core.ServerConfiguration) {
-	go func() {
-		absolutePath, err := filepath.Abs(sc.LocalBasePath)
-		if err != nil {
-			log.Printf("Error Starting File Cleaner - Unable to create absolute path [%s]", sc.LocalBasePath)
-		} else {
-			var stat, _ = os.Stat(absolutePath)
-			if absolutePath != "" && stat.IsDir() {
-				log.Printf("Starting File Cleaner on path [%s]", absolutePath)
-				filepath.Walk(absolutePath, func(path string, info os.FileInfo, err error) error {
-					if !info.IsDir() {
-						age := time.Now().Sub(info.ModTime())
-						log.Printf("Workspace Contains at Startup file [%s] size [%d] modTime [%s] age [%s]\n", path, info.Size(), info.ModTime(), age)
-					}
-					return nil
-				})
-				for range sc.CleanUpTicker.C {
-					filepath.Walk(absolutePath, func(path string, info os.FileInfo, err error) error {
-						if !info.IsDir() {
-							age := time.Now().Sub(info.ModTime())
-							if age > sc.MaxFileAge {
-								log.Printf("Deleting file [%s] size [%d] modTime [%s] age [%s]\n", path, info.Size(), info.ModTime(), age)
-								var err = os.Remove(path)
-								if err != nil {
-									log.Printf("Error deleting file [%s]\n", path)
-								}
-							}
-						}
-						return nil
-					})
-				}
-			} else {
-				log.Printf("Error Starting File Cleaner - Invalid walk path [%s]", absolutePath)
-			}
-		}
-	}()
 }
 
 func serverConfiguration() (*core.ServerConfiguration, error) {
